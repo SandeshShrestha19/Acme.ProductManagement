d70b16883b45aba06615011a4c5cb9842734b16e
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:43:15 +0545

    feat: implement interface for product service

---
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Acme.ProductManagement.DTOs;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.ProductManagement.Interfaces
+{
+    public interface IProductService : ICrudAppService< 
+        ProductDto, 
+        Guid, 
+        PagedAndSortedResultRequestDto, 
+        CreateUpdateProductDto>
+    {
+
+    }
+    //you can inherit from IApplicationService and create your own methods.
+}
