3fcadcaaa4cdab9da1121d5a9cf882995f09767e
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:41:58 +0545

    feat: implement interface for category service

---
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Acme.ProductManagement.DTOs;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.ProductManagement.Interfaces
+{
+    public interface ICategoryService : ICrudAppService< //Defines CRUD methods
+        CategoryDto, 
+        Guid,
+        PagedAndSortedResultRequestDto,
+        CreateUpdateCategoryDto>
+    {
+    }
+}
