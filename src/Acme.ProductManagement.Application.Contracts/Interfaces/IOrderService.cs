fb04b5edd9e0c354fedcd8da1b8357ff4874d815
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:41:15 +0545

    feat: implement interface for order service

---
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/IOrderService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.ProductManagement.DTOs.OrderDto;
+using Acme.ProductManagement.Enums;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+
+namespace Acme.ProductManagement.Interfaces
+{
+    public interface IOrderService : ICrudAppService< 
+        OrderDto, 
+        Guid, 
+        PagedAndSortedResultRequestDto, 
+        CreateOrderDto,
+        UpdateOrderDto>
+    {
+    }
+}
