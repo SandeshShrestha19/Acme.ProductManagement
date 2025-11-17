870e734f1831ad6ed9515d308b37e7d112616c1b
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:38:37 +0545

    feat: add dto to handle product related operation

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.ProductManagement.DTOs
+{
+    public class ProductDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Guid CategoryId { get; set; }
+        public int Stock {  get; set; }
+    }
+}
