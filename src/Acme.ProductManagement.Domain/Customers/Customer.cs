72f200e1fa7ed2c467c6be49482d0a397562b0d5
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:38:12 +0545

    feat: add properties for customer entity

--- src/Acme.ProductManagement.Domain/Customers/Customer.cs
+++ src/Acme.ProductManagement.Domain/Customers/Customer.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Acme.ProductManagement.Customers
@@ -11,5 +8,12 @@ namespace Acme.ProductManagement.Customers
     {
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
+        public Customer(Guid id, string fullName, string phoneNumber) : base(id)
+        {
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+        }
+
+        protected Customer() { }
     }
 }
