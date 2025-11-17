f5f4f42892981a89f3bc8fb9788807eb363734ea
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:37:32 +0545

    feat: add properties for orderItem entity

---
+++ src/Acme.ProductManagement.Domain/OrderItems/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Acme.ProductManagement.Products;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities;
+
+namespace Acme.ProductManagement.OrderItems
+{
+    public class OrderItem : Entity<Guid>
+    {
+        public Guid OrderId { get; private set; }
+        public Guid ProductId { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+
+        protected OrderItem() { } 
+
+        public OrderItem(Guid id, Guid orderId, Guid productId, int quantity, decimal unitPrice)
+            : base(id)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+    }
+}
