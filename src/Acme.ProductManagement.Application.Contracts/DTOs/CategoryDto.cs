89ef7309c5fed1819b7ee9d49041d0538f789985
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:31:35 +0545

    feat: add dto for category related operation

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CategoryDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.ProductManagement.DTOs
+{
+    public class CategoryDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
