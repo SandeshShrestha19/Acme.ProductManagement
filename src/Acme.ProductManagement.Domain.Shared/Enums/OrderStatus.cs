9a38b5ba42243176da9d090be15c559411c48004
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:35:46 +0545

    feat: add enum for checking order status

---
+++ src/Acme.ProductManagement.Domain.Shared/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Acme.ProductManagement.Enums
+{
+    public enum OrderStatus
+    {
+        Draft = 0,
+        Placed = 1,
+        Paid = 2,
+        Shipped = 3,
+        Completed = 4,
+        Cancelled = 5,
+        Pending = 6
+    }
+}
