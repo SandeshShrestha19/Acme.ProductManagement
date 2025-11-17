f7c91ab6aaaba580d783f7295c7b648f274d7f86
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:36:09 +0545

    feat: add dto to place a order

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/ItemsDto/OrderItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Acme.ProductManagement.DTOs.ItemsDto
+{
+    public class OrderItemDto
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } 
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice => Quantity * UnitPrice;
+    }
+}
