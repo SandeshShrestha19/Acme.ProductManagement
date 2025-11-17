59e3d65245774b4bb44a07a841b88df40a998ee3
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:41:39 +0545

    feat: implement interface for customer service

---
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Acme.ProductManagement.DTOs.CustomersDto;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.ProductManagement.Interfaces
+{
+    public interface ICustomerService : ICrudAppService<CustomerDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateCustomerDto>
+    {
+    }
+}
