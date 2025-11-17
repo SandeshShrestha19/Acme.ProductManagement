61167a897fc0efcf787313db639c5907687198e4
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:39:58 +0545

    feat: implement service for orderItem

---
+++ src/Acme.ProductManagement.Application/Services/OrderItemService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Acme.ProductManagement.DTOs.ItemsDto;
+using Acme.ProductManagement.DTOs.OrderDto;
+using Acme.ProductManagement.DTOs.OrderItemDto;
+using Acme.ProductManagement.Interfaces;
+using Acme.ProductManagement.OrderItems;
+using Acme.ProductManagement.Orders;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.ProductManagement.Services
+{
+    public class OrderItemService : CrudAppService<
+        OrderItem,
+        OrderItemDto,
+        Guid,
+        PagedAndSortedResultRequestDto,
+        CreateOrderItemDto,
+        UpdateOrderItemDto>, IOrderItemService
+    {
+        public OrderItemService(IRepository<OrderItem, Guid> orderItemRepository)
+        : base(orderItemRepository)
+        {
+
+        }
+    }
+}
