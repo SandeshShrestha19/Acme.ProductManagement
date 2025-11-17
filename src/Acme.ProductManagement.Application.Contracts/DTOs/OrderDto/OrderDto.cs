e3187ee94b2a0710065c5900ff0da96db56b3adf
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:36:56 +0545

    feat: add dto to handle order related operation

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/OrderDto/OrderDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.ProductManagement.Enums;
+using Acme.ProductManagement.DTOs.ItemsDto;
+
+
+namespace Acme.ProductManagement.DTOs.OrderDto
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; } 
+        public DateTime OrderDate { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public string OrderStatusDisplay => OrderStatus.ToString();
+        public decimal TotalAmount { get; set; }
+    }
+}
