86e2e4e226f43762f02cd6e4af26d796084d37e6
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:37:09 +0545

    feat: add dto to create order

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/OrderDto/CreateOrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.ProductManagement.DTOs.OrderItemDto;
+
+namespace Acme.ProductManagement.DTOs.OrderDto
+{
+    public class CreateOrderDto
+    {
+        [Required]
+        public Guid CustomerId { get; set; }
+
+        public List<CreateOrderItemDto> OrderItems { get; set; } = new();
+    }
+}
