c2b0b727931f90bb863f7f8bb90db71a1753cf68
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:33:56 +0545

    feat: add dto to create an order for single item

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/ItemsDto/CreateOrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Acme.ProductManagement.DTOs.OrderItemDto
+{
+    public class CreateOrderItemDto
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
