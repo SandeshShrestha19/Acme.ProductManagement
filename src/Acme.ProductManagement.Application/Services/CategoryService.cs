32546e40aa4d6f4ed4c43d919e4f071e608a7d0a
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:38:53 +0545

    feat: add service for category

---
+++ src/Acme.ProductManagement.Application/Services/CategoryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.ProductManagement.Categories;
+using Acme.ProductManagement.DTOs;
+using Acme.ProductManagement.Interfaces;
+using Acme.ProductManagement.Products;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.ProductManagement.Services
+{
+    public class CategoryService : CrudAppService<Category, CategoryDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateCategoryDto>, ICategoryService
+    {
+        public CategoryService(IRepository<Category, Guid> categoryRepository) : base(categoryRepository) { }
+    }
+}
