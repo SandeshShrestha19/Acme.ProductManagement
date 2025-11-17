89ed3362eb170f993798e519135e437da7681938
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:42:20 +0545

    feat: implement interface for orderItem service

---
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/IOrderItemService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.ProductManagement.DTOs.ItemsDto;
+using Acme.ProductManagement.DTOs.OrderDto;
+using Acme.ProductManagement.DTOs.OrderItemDto;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+
+namespace Acme.ProductManagement.Interfaces
+{
+    public interface IOrderItemService : ICrudAppService<
+        OrderItemDto,
+        Guid,
+        PagedAndSortedResultRequestDto,
+        CreateOrderItemDto,
+        UpdateOrderItemDto>
+    {
+    }
+}
