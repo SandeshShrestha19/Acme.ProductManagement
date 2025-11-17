9dc14c2f37a8e887dd394b4374d355d7f1ab4b08
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:31:57 +0545

    feat: add dto for creating an updating category

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CreateUpdateCategoryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.ProductManagement.DTOs
+{
+    public class CreateUpdateCategoryDto
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(1028)]
+        public string? Description { get; set; }
+    }
+}
