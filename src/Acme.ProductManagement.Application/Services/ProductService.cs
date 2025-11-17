93009fc99ef740a41d7bf54ae0d8906ae4664044
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:37:56 +0545

    feat: implement service for product

---
+++ src/Acme.ProductManagement.Application/Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Acme.ProductManagement.DTOs;
+using Acme.ProductManagement.Interfaces;
+using Acme.ProductManagement.Products;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Linq;
+
+namespace Acme.ProductManagement.Services
+{
+    public class ProductService : CrudAppService<
+        Product, 
+        ProductDto, 
+        Guid, 
+        PagedAndSortedResultRequestDto, 
+        CreateUpdateProductDto>, IProductService
+    {
+        public ProductService(IRepository<Product, Guid> productRepository) : base(productRepository) { }
+    }
+
+        
+
+
+    
+}
