2dfc98d9079dec8b9e84711e531ca7b897d768fc
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-17 13:40:18 +0545

    feat: add service for customer

---
+++ src/Acme.ProductManagement.Application/Services/CustomerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.ProductManagement.Customers;
+using Acme.ProductManagement.DTOs.CustomersDto;
+using Acme.ProductManagement.Interfaces;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.ProductManagement.Services
+{
+    public class CustomerService : CrudAppService<Customer, CustomerDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateCustomerDto>, ICustomerService
+    {
+        public CustomerService(IRepository<Customer, Guid> customerRepository) : base(customerRepository) { }
+    }
+}
