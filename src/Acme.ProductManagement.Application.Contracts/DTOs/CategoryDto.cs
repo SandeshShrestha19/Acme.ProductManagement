8bec8592318f587834afbfc5ca2f92db5fc8b3f2
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:28:37 +0545

    feat: add dto for category entity

--- src/Acme.ProductManagement.Application.Contracts/DTOs/CategoryDto.cs
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CategoryDto.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Acme.ProductManagement.Categories;
 using Volo.Abp.Application.Dtos;
 
 namespace Acme.ProductManagement.DTOs
 {
-    public class CategoryDto : AuditedEntityDto<Guid>
+    public class CategoryDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
-
         public string Description { get; set; }
+
+        public CategoryDto()
+        {
+        }
+
+        public CategoryDto(Category category)
+        {
+            Id = category.Id;
+            Name = category.Name;
+            Description = category.Description;
+        }
     }
 }
