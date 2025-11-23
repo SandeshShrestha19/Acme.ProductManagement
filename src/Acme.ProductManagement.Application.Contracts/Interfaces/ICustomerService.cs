aefe6324d9ef531df3cb1e41810036b2d75a7794
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:33:48 +0545

    feat: implement interface for customer service

--- src/Acme.ProductManagement.Application.Contracts/Interfaces/ICustomerService.cs
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/ICustomerService.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Acme.ProductManagement.DTOs.CustomersDto;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace Acme.ProductManagement.Interfaces
 {
-    public interface ICustomerService : ICrudAppService<CustomerDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateCustomerDto>
+    public interface ICustomerService : IApplicationService
     {
+        Task CreateAsync(CreateUpdateCustomerDto createDto);
+        Task UpdateAsync(Guid customerId, CreateUpdateCustomerDto updateDto);
+        Task DeleteAsync(Guid customerId);
+        Task<CustomerDto> GetAsync(Guid customerId);
     }
 }
