f1e4b77480c6f729e10b64c3940f104bdff243b6
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:37:21 +0545

    feat: add properties for product entity

--- src/Acme.ProductManagement.Domain/Products/Product.cs
+++ src/Acme.ProductManagement.Domain/Products/Product.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Acme.ProductManagement.Categories;
+using Acme.ProductManagement.Inventories;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Acme.ProductManagement.Products
 {
     public class Product : AuditedAggregateRoot<Guid>
     {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Description { get; set; }
-        public Guid CategoryId { get; set; }
-        public Category Category { get; set; }
-        public int Stock { get; set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string Description { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public Category Category { get; private set; }
+        public Inventory Inventory { get; private set; }
+        protected Product() { }
+
+        public Product(string name, decimal price, string description, Guid categoryId)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            CategoryId = categoryId;
+        }
+        public void Update(string name, decimal price, string description, Guid categoryId)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            CategoryId = categoryId;
+        }
     }
 }
