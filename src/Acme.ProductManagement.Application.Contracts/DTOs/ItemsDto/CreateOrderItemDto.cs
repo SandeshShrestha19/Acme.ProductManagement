4d01cc51a5ea9458376b52fec0a15d0fcfbbd67f
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:31:09 +0545

    feat: add dto to create orderItem

--- src/Acme.ProductManagement.Application.Contracts/DTOs/ItemsDto/CreateOrderItemDto.cs
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/ItemsDto/CreateOrderItemDto.cs
@@ -6,6 +6,5 @@ namespace Acme.ProductManagement.DTOs.OrderItemDto
     {
         public Guid ProductId { get; set; }
         public int Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
     }
 }
