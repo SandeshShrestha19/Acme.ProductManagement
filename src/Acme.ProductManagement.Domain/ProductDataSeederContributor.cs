ce2a595e434f88e6a57add2977d20cb826a9eef0
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:42:35 +0545

    feat: add initial data in database using data seeder

--- src/Acme.ProductManagement.Domain/ProductDataSeederContributor.cs
+++ src/Acme.ProductManagement.Domain/ProductDataSeederContributor.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Acme.ProductManagement.Categories;
 using Acme.ProductManagement.Customers;
+using Acme.ProductManagement.Inventories;
 using Acme.ProductManagement.Orders;
 using Acme.ProductManagement.Products;
 using Volo.Abp.Data;
@@ -17,85 +16,89 @@ namespace Acme.ProductManagement
     {
         private readonly IRepository<Product, Guid> _productRepository;
         private readonly IRepository<Category, Guid> _categoryRepository;
-        private readonly IRepository<Order, Guid> _orderRepository;
+        private readonly IRepository<Orders.Order, Guid> _orderRepository;
         private readonly IRepository<Customer, Guid> _customerRepository;
+        private readonly IRepository<Inventory, Guid> _inventoryRepository;
 
         public ProductDataSeederContributor(
             IRepository<Product, Guid> productRepository,
             IRepository<Category, Guid> categoryRepository,
-            IRepository<Order, Guid> orderRepository,
-            IRepository<Customer, Guid> customerRepository)
+            IRepository<Orders.Order, Guid> orderRepository,
+            IRepository<Customer, Guid> customerRepository,
+            IRepository<Inventory, Guid> inventoryRepository)
         {
             _productRepository = productRepository;
             _categoryRepository = categoryRepository;
             _orderRepository = orderRepository;
             _customerRepository = customerRepository;
+            _inventoryRepository = inventoryRepository;
         }
 
         public async Task SeedAsync(DataSeedContext context)
         {
             if (await _customerRepository.GetCountAsync() == 0)
             {
-                var customers = new List<Customer>
-                {
-                    new Customer(id: Guid.NewGuid(),fullName: "John Smith",phoneNumber: "+977-9876561424"),
-                    new Customer(id: Guid.NewGuid(),fullName: "Pyarey Shrestha",phoneNumber: "+977-9876456424"),
-                    new Customer(id: Guid.NewGuid(),fullName: "Mike Williams",phoneNumber: "+977-984193424"),
-                    new Customer(id : Guid.NewGuid(), fullName : "Hati King", phoneNumber : "+977-9876522224"),
-                };
-
-                foreach (var customer in customers)
-                {
-                    await _customerRepository.InsertAsync(customer, autoSave: false);
-                }
-                await _customerRepository.InsertManyAsync(customers);
+                await _customerRepository.InsertManyAsync(new List<Customer>
+            {
+            new Customer("John Smith", "+977-9876561424"),
+            new Customer("Pyarey Shrestha", "+977-9876456424"),
+            new Customer("Mike Williams", "+977-984193424"),
+            new Customer("Hati King", "+977-9876522224")
+            }, autoSave: true);
             }
+
+            Category liquor = null;
+            Category electronics = null;
 
             if (await _categoryRepository.GetCountAsync() == 0)
             {
-                var liquor = await _categoryRepository.InsertAsync(new Category
+                liquor = await _categoryRepository.InsertAsync(
+                    new Category("Liquor", "Alcohol & Beverages"), true);
+
+                electronics = await _categoryRepository.InsertAsync(
+                    new Category("Electronics", "Gadgets & Devices"), true);
+            }
+            else
+            {
+                var categories = await _categoryRepository.GetListAsync();
+                liquor = categories.Find(c => c.Name == "Liquor");
+                electronics = categories.Find(c => c.Name == "Electronics");
+            }
+
+            Product nepalIce = null;
+            Product samsungS24 = null;
+
+            if (await _productRepository.GetCountAsync() == 0)
+            {
+                nepalIce = await _productRepository.InsertAsync(
+                    new Product("NepalIce", 2.33m, "Nepal's finest beer.", liquor.Id),
+                    true);
+
+                await _inventoryRepository.InsertAsync(
+                    new Inventory(nepalIce.Id, 100),
+                    true);
+
+                samsungS24 = await _productRepository.InsertAsync(
+                    new Product("Samsung S24 Ultra", 1999.99m, "Flagship phone", electronics.Id),
+                    true);
+
+                await _inventoryRepository.InsertAsync(
+                    new Inventory(samsungS24.Id, 50),
+                    true);
+            }
+
+            if (await _orderRepository.GetCountAsync() == 0)
+            {
+                var customers = await _customerRepository.GetListAsync();
+                if (customers.Count > 0 && nepalIce != null)
                 {
-                    Name = "Liquor",
-                    Description = "Alcohol & Beverages"
-                }, autoSave: true);
+                    var customer = customers[0];
 
-                var electronics = await _categoryRepository.InsertAsync(new Category
-                {
-                    Name = "Electronics",
-                    Description = "Gadgets & Devices"
-                }, autoSave: true);
+                    var order = new Orders.Order(customer.Id);
+                    order.AddItem(nepalIce, 3);
+                    order.AddItem(samsungS24, 1);
 
-                if (await _productRepository.GetCountAsync() == 0)
-                {
-                    var nepalIce = await _productRepository.InsertAsync(new Product
-                    {
-                        Name = "Nepal Ice",
-                        Price = 2.33m,
-                        Description = "Nepal's finest beer.",
-                        CategoryId = liquor.Id,
-                        Stock = 200
-                    }, autoSave: true);
-
-                    var samsungS24 = await _productRepository.InsertAsync(new Product
-                    {
-                        Name = "Samsung S24 Ultra",
-                        Price = 1999.99m,
-                        Description = "Flagship phone",
-                        CategoryId = electronics.Id,
-                        Stock = 50
-                    }, autoSave: true);
-
-                    var customers = await _customerRepository.GetListAsync();
-                    if (customers.Count > 0 && await _orderRepository.GetCountAsync() == 0)
-                    {
-                        var customer = customers[0]; // Use first customer
-                        var order = new Order(Guid.NewGuid(), customer.Id);
-
-                        order.AddItem(nepalIce.Id, 3, nepalIce.Price);
-                        order.AddItem(samsungS24.Id, 1, samsungS24.Price);
-
-                        await _orderRepository.InsertAsync(order, autoSave: true);
-                    }
+                    await _orderRepository.InsertAsync(order, true);
                 }
             }
         }
