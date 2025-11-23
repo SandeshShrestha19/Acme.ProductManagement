a3d22cc0172eb79bda75fc8ace78fb89464b3d7e
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:28:57 +0545

    feat: add dto for customer entity

--- src/Acme.ProductManagement.Application.Contracts/DTOs/CustomersDto/CustomerDto.cs
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CustomersDto/CustomerDto.cs
@@ -1,16 +1,24 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using Acme.ProductManagement.Customers;
 using Volo.Abp.Application.Dtos;
 
 namespace Acme.ProductManagement.DTOs.CustomersDto
 {
-    public class CustomerDto : AuditedEntityDto<Guid>
+    public class CustomerDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
+
+        public CustomerDto()
+        {
+        }
+
+        public CustomerDto(Customer customer)
+        {
+            Id = customer.Id;
+            Name = customer.FullName;
+            PhoneNumber = customer.PhoneNumber;
+        }
     }
 }
