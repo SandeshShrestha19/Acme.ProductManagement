44002903bb316bb466c8017505be1a5ae75ca48b
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:36:34 +0545

    feat: implement interface for category service

--- src/Acme.ProductManagement.Application.Contracts/Interfaces/ICategoryService.cs
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/ICategoryService.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Acme.ProductManagement.DTOs;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace Acme.ProductManagement.Interfaces
 {
-    public interface ICategoryService : ICrudAppService< //Defines CRUD methods
-        CategoryDto, 
-        Guid,
-        PagedAndSortedResultRequestDto,
-        CreateUpdateCategoryDto>
+    public interface ICategoryService : IApplicationService
     {
+        Task CreateAsync(CreateUpdateCategoryDto createDto);
+        Task UpdateAsync(Guid categoryId, CreateUpdateCategoryDto upadteDto);
+        Task DeleteAsync(Guid categoryId);
+
+        Task<CategoryDto> GetAsync(Guid categoryId);
+        Task<List<CategoryDto>> GetListAsync();
     }
 }
