f39a4f7f62ca3716d9d757851ea1c75347d78e13
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:38:06 +0545

    feat: implement interface for inventory service

---
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/IInventoryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.ProductManagement.DTOs.Inventory;
+using Volo.Abp.Application.Services;
+
+namespace Acme.ProductManagement.Interfaces
+{
+    public interface IInventoryService : IApplicationService
+    {
+        Task IncreaseStockAsync(Guid productId, UpdateInventoryDto increaseDto);
+        Task DecreaseStockAsync(Guid productId, UpdateInventoryDto decreaseDto);
+    }
+}
