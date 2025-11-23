2a19aba6715e88c9ac0fc8129e95ca1146cedbcc
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:33:19 +0545

    feat: add dto to update order

--- src/Acme.ProductManagement.Application.Contracts/DTOs/OrderDto/UpdateOrderDto.cs
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/OrderDto/UpdateOrderDto.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Acme.ProductManagement.DTOs.OrderItemDto;
-using Acme.ProductManagement.Enums;
 
 namespace Acme.ProductManagement.DTOs.OrderDto
 {
     public class UpdateOrderDto
     {
-        public OrderStatus OrderStatus { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
 
         public List<UpdateOrderItemDto> OrderItems { get; set; } = new();
     }
