9825aa7aed0323a5cf537d689ad9b40c4809c40b
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:39:21 +0545

    feat: add service for order entity

--- src/Acme.ProductManagement.Application/Services/OrderService.cs
+++ src/Acme.ProductManagement.Application/Services/OrderService.cs
@@ -1,34 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Acme.ProductManagement.Customers;
 using Acme.ProductManagement.DTOs.OrderDto;
-using Acme.ProductManagement.DTOs.OrderItemDto;
-using Acme.ProductManagement.Enums;
 using Acme.ProductManagement.Interfaces;
-using Acme.ProductManagement.OrderItems;
+using Acme.ProductManagement.Order;
 using Acme.ProductManagement.Orders;
 using Acme.ProductManagement.Products;
-using AutoMapper.Internal.Mappers;
-using Volo.Abp.Application.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Repositories;
 
 namespace Acme.ProductManagement.Services
 {
-    public class OrderService : CrudAppService<
-        Order, 
-        OrderDto,
-        Guid, 
-        PagedAndSortedResultRequestDto, 
-        CreateOrderDto,
-        UpdateOrderDto>, IOrderService
+    public class OrderService : ApplicationService, IOrderService, ITransientDependency
     {
-        public OrderService(IRepository<Order, Guid> orderRepository)
-        : base(orderRepository)
+        private readonly IRepository<Orders.Order, Guid> _orderRepository;
+        private readonly IRepository<Product, Guid> _productRepository;
+        private readonly ILogger<OrderService> _logger;
+
+        public OrderService(IRepository<Orders.Order, Guid> orderRepository, IRepository<Product, Guid> productRepository, ILogger<OrderService> logger)
         {
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task CreateAsync(CreateOrderDto createDto)
+        {
+            try
+            {
+                var order = new Orders.Order(customerId: createDto.CustomerId);
+                await _orderRepository.InsertAsync(order);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Error occurred while creating a order");
+                throw;
+            }
+        }
+        [HttpPut("order/{orderId}")]
+        public async Task UpdateAsync(Guid orderId, UpdateOrderDto updateDto)
+        {
+            try
+            {
+                var order = await _orderRepository.GetAsync(orderId);
+                var product = await _productRepository.GetAsync(updateDto.ProductId);
+                order.AddItem(product: product, quantity: updateDto.Quantity);
+                await _orderRepository.UpdateAsync(order);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Error occurred while updating order");
+                throw;
+            }
+        }
+        [HttpDelete("order/{orderId}")]
+        public async Task DeleteAsync(Guid orderId)
+        {
+            try
+            {
+                var order = await _orderRepository.GetAsync(orderId);
+                await _orderRepository.DeleteAsync(order);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Error occurred while deleting order");
+                throw;
+            }
+
         }
     }
 }
