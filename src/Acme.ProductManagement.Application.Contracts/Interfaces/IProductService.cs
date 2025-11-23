4c378c22593fa7e97553087b6bbcc27ca2e2f6b7
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-23 16:38:27 +0545

    feat: implement interface for product service

--- src/Acme.ProductManagement.Application.Contracts/Interfaces/IProductService.cs
+++ src/Acme.ProductManagement.Application.Contracts/Interfaces/IProductService.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Acme.ProductManagement.DTOs;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace Acme.ProductManagement.Interfaces
 {
-    public interface IProductService : ICrudAppService< 
-        ProductDto, 
-        Guid, 
-        PagedAndSortedResultRequestDto, 
-        CreateUpdateProductDto>
+    public interface IProductService : IApplicationService
     {
+        Task CreateAsync(CreateUpdateProductDto createDto);
+        Task UpdateAsync(Guid productId, CreateUpdateProductDto updateDto);
+        Task DeleteAsync(Guid productId);
 
+        Task<ProductDto> GetAsync(Guid productId);
+        Task<List<ProductDto>> GetListAsync();
     }
-    //you can inherit from IApplicationService and create your own methods.
 }
