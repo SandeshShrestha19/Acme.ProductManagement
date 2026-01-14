67a40dfddeae8c09e5f79aac76f37e12571c74ed
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:35:59 +0545

    fix: added new properties for Customer related operation

--- src/Acme.ProductManagement.Domain/Customers/Customer.cs
+++ src/Acme.ProductManagement.Domain/Customers/Customer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Acme.ProductManagement.Orders;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Acme.ProductManagement.Customers
@@ -6,18 +8,29 @@ namespace Acme.ProductManagement.Customers
     public class Customer : AuditedAggregateRoot<Guid>
     {
         public string FullName { get; set; }
-        public string PhoneNumber { get; protected set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+        protected Customer() { }
+
         public Customer(string fullName, string phoneNumber)
         {
             FullName = fullName;
             PhoneNumber = phoneNumber;
         }
-        protected Customer() { }
 
         public void Update(string fullName, string phoneNumber)
         {
-            FullName = fullName;
-            PhoneNumber = phoneNumber;
+            if (!string.IsNullOrWhiteSpace(fullName))
+            {
+                FullName = fullName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                PhoneNumber = phoneNumber;
+            }
         }
     }
 }
