e60ce16910e51e7cc49a76710bc1c3b5ca308d73
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:32:34 +0545

    feat: add dto properties for category

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CategoriesDto/CategoryDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Acme.ProductManagement.Categories;
+using Acme.ProductManagement.DTOs.ProductsDto;
+using Acme.ProductManagement.Orders;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.ProductManagement.DTOs.CategoriesDto
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<ProductDto> Products { get; set; } = new List<ProductDto>();
+
+        protected CategoryDto()
+        {
+        }
+
+        public CategoryDto(Category category)
+        {
+            Id = category.Id;
+            Name = category.Name;
+            Description = category.Description;
+            Products = category.Products?.Select(product => new ProductDto(product)).ToList() ?? new List<ProductDto>();
+        }
+    }
+}
