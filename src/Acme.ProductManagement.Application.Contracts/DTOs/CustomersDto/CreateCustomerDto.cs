3fc56c3eac530dfaeab9f344e1051917c7a5e746
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:33:39 +0545

    feat: add properties to create and update Customer

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CustomersDto/CreateCustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.ProductManagement.DTOs.CustomersDto
+{
+    public class CreateCustomerDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CustomersDto/UpdateCustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.ProductManagement.DTOs.CustomersDto
+{
+    public class UpdateCustomerDto
+    {
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [StringLength(20)]
+        public string? PhoneNumber { get; set; }
+    }
+}
