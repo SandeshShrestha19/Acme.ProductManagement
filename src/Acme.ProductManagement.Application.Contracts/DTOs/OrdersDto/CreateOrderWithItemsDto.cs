ab96119151eea793e6e6c46514c68fd72b776875
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:45:33 +0545

    feat: created dto to create orders with orderItems

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/OrdersDto/CreateOrderWithItemsDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Acme.ProductManagement.DTOs.OrderDto
+{
+    public class CreateOrderWithItemsDto
+    {
+        public Guid CustomerId { get; set; }
+        public List<AddOrderItemDto> Items { get; set; } = new List<AddOrderItemDto>();
+    }
+}
