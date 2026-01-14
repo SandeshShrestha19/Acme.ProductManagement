53923b2337bcf708171732bef9f38c153d08ade6
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:33:06 +0545

    feat: add properties to create and update category

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/CategoriesDto/UpdateCategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.ProductManagement.DTOs.CategoriesDto
+{
+    public class UpdateCategoryDto
+    {
+        [StringLength(128)]
+        public string? Name { get; set; } = string.Empty;
+
+        [StringLength(1028)]
+        public string? Description { get; set; }
+    }
+}
