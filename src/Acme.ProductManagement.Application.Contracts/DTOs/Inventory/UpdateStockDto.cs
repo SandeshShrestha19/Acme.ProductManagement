e9d728f03e7bfa3c6d8e78143f4064ba1ca26ce8
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:41:18 +0545

    feat: add dto to update stock of product

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/Inventory/UpdateStockDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.ProductManagement.DTOs.Inventory
+{
+    public class UpdateStockDto
+    {
+        public int Quantity { get; set; }
+    }
+}
