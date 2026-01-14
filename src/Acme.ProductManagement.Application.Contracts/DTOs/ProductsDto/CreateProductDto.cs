fb724f2fee1930d1ff6f162835d5e16797cb8628
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:45:49 +0545

    feat: add dto to create product

---
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/ProductsDto/CreateProductDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.ProductManagement.DTOs.ProductsDto
+{
+    public class CreateProductDto
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CurrentStock { get; set; }
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public Guid CategoryId { get; set; }
+    }
+}
