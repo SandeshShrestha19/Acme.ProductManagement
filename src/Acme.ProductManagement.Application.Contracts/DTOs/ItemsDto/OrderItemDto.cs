b8e7579b4900a6de213406e8750c4ef59c1d53bc
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2026-01-14 17:43:06 +0545

    feat: created dto for orderItem entity

--- src/Acme.ProductManagement.Application.Contracts/DTOs/ItemsDto/OrderItemDto.cs
+++ src/Acme.ProductManagement.Application.Contracts/DTOs/ItemsDto/OrderItemDto.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using Acme.ProductManagement.Orders;
+using Acme.ProductManagement.OrderItems;
+using Acme.ProductManagement.Products;
+using System.Net.Sockets;
 
 namespace Acme.ProductManagement.DTOs.ItemsDto
 {
     public class OrderItemDto
     {
         public Guid Id { get; set; }
-        public Guid OrderId { get; set; }
-        public Guid ProductId { get; set; }
         public string ProductName { get; set; }
-        public decimal ProductPrice { get; set; }
+        public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public decimal TotalPrice => Quantity * ProductPrice;
+        public decimal TotalPrice { get; set; }
 
         public OrderItemDto()
         {
         }
+
+        public OrderItemDto(OrderItem orderItem)
+        {
+            Id = orderItem.Id;
+            ProductName = orderItem.ProductName;
+            Price = orderItem.UnitPrice;  
+            Quantity = orderItem.Quantity;
+            TotalPrice = orderItem.TotalPrice;
+        }
     }
+
 }
